f40b66f5c89d4e8a61518e1f2183a27ad6ed6de6
bhanukabandara12 <bhanuka2002bandara@gmail.com>
2024-12-09 15:15:21 +0530

    forms connect to database

--- lib/views/dashboard/commment.php
+++ lib/views/dashboard/commment.php
@@ -1,3 +1,50 @@
+<?php
+include("../../functions/connect_db.php");
+
+$name = $position = $email = $message = $photoFileName = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = mysqli_real_escape_string($con, $_POST['name']);
+    $position = mysqli_real_escape_string($con, $_POST['position']);
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $message = mysqli_real_escape_string($con, $_POST['message']);
+
+    if (isset($_FILES['photo']) && $_FILES['photo']['error'] === UPLOAD_ERR_OK) {
+        $photoTmpName = $_FILES['photo']['tmp_name'];
+        $photoFileName = basename($_FILES['photo']['name']);
+        $uploadDir = "uploads/photos/";
+        $uploadFilePath = $uploadDir . $photoFileName;
+
+        if (!is_dir($uploadDir)) {
+            mkdir($uploadDir, 0777, true);
+        }
+
+        if (move_uploaded_file($photoTmpName, $uploadFilePath)) {
+            // File uploaded successfully
+        } else {
+            echo "Error uploading photo.";
+            exit();
+        }
+    } else {
+        echo "Photo upload error.";
+        exit();
+    }
+
+    $sql = "INSERT INTO comments (name, position, email, photo, message) 
+            VALUES ('$name', '$position', '$email', '$photoFileName', '$message')";
+
+    if (mysqli_query($con, $sql)) {
+        echo "<script>alert('Comment added successfully');</script>";
+    } else {
+        echo "Error: " . mysqli_error($con);
+    }
+}
+
+mysqli_close($con);
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -57,30 +104,37 @@
 <div class="container-fluid col-md-6 form-outline">
   <h1 class="text-center" style="font-weight: bold;">Add a Your Comment</h1>
 
-  <form>
+  <form method = "post" action="commment.php" enctype="multipart/form-data">
       <div class="form-group">
           <i class="fas fa-user"></i>
           <label for="firstName">Your Name</label>
-          <input type="text" class="form-control" id="firstName" required>
+          <input type="text" class="form-control" id="firstName" name = "name" required>
       </div>
        
       <div class="form-group">
           <i class="fas fa-user-tie"></i>
           <label for="firstName">Your Position</label>
-          <input type="text" class="form-control" id="firstName" required>
+          <input type="text" class="form-control" id="firstName" name = "position" required>
       </div>
      
 
       <div class="form-group">
           <i class="fas fa-envelope"></i>
           <label for="email">Email</label>
-          <input type="email" class="form-control" id="email" required>
+          <input type="email" class="form-control" id="email" name = "email" required>
       </div>
       
       <div class="form-group">
+        <i class="fas fa-camera"></i>
+        <label for="uploadPhoto">Upload Your Photo</label>
+        <input type="file" class="form-control" id="uploadPhoto" name = "photo"accept="image/*" required>
+      </div>
+
+
+      <div class="form-group">
           <i class="fas fa-comment"></i>
           <label for="message">Comment</label>
-          <textarea class="form-control" id="message" rows="4" placeholder="Message"></textarea>
+          <textarea class="form-control" id="message" rows="4" placeholder="Message" name = "message"></textarea>
       </div>
       <br><br>
       <input class="btn btn-primary w-100 py-2" type="submit" value="Submit">
